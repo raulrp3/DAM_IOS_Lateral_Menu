f09465b3793c21a39a77dcb8dacf1cae8f3157e8
raulrp3 <raul.ramirez@escuelaestech.es>
2020-01-09 16:54:28 +0100

    Creados dos menús laterales

--- DAM_IOS_Lateral_Menu/ViewController.swift
+++ DAM_IOS_Lateral_Menu/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    @IBOutlet weak var rightButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.sideMenus()
+        self.customizeNavBar()
     }
-
-
+    
+    func sideMenus(){
+        if revealViewController() != nil{
+            menuButton.target = revealViewController()
+            menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            
+            rightButton.target = revealViewController()
+            rightButton.action = #selector(SWRevealViewController.rightRevealToggle(_:))
+            
+            revealViewController()?.rearViewRevealWidth = 276
+            revealViewController()?.rightViewRevealWidth = 160
+            
+        view.addGestureRecognizer((self.revealViewController()?.panGestureRecognizer())!)
+        }
+    }
+    
+    func customizeNavBar(){
+        navigationController?.navigationBar.tintColor = UIColor.white
+        navigationController?.navigationBar.barTintColor = UIColor(red: 50/255, green: 170/255, blue: 196/255, alpha: 1)
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+    }
 }
 
